bc7855262d8900f3772e22f3598f185c693800cc
Ownsky <wangguoyu7777@me.com>
2017-10-22 22:15:31 +0800

    ignore xml configs

--- src/main/java/org/bugjlu/mycollection/service/TagService.java
+++ src/main/java/org/bugjlu/mycollection/service/TagService.java
@@ -5,9 +5,9 @@ import org.springframework.stereotype.Service;
 
 @Service
 public interface TagService {
-    void addTag(Tag tag);
+    Tag addTag(Tag tag);
     void removeTag(Integer tagid);
-    void modifyTag(Integer tagid, Tag newTag);
+    Tag modifyTag(Integer tagid, Tag newTag);
     void addContentToTag(Integer contentId, Integer tagId);
     void removeContentFromTag(Integer contentId, Integer tagId);
 }
